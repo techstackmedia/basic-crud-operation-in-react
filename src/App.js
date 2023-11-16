813a49dc251c096a65a580f642470d43ad8ee939
Bello Osagie <osagienoah04@gmail.com>
2023-11-15 17:13:38 -0800

    basic crud operation on card component - including improved search functionality with pagination

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import { useState } from "react";
-import Card from "./components/Card";
-import "./App.css";
-import cardData from "./data";
-import AddButton from "./components/AddButton";
-import Search from "./components/Search";
-import logo from './logo.svg'
+import { useState } from 'react';
+import Card from './components/Card';
+import './App.css';
+import cardData from './data';
+import AddButton from './components/AddButton';
+import Search from './components/Search';
+import logo from './logo.svg';
+
+const ITEMS_PER_PAGE = 6;
 
 export default function App() {
-  const [data, setData] = useState(cardData);
+  const [originalData, setOriginalData] = useState(cardData);
+  const [data, setData] = useState(originalData);
+  const [currentPage, setCurrentPage] = useState(1);
+  const indexOfLastItem = currentPage * ITEMS_PER_PAGE;
+  const indexOfFirstItem = indexOfLastItem - ITEMS_PER_PAGE;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
   const removeCard = (id) => {
     setData(data.filter((item) => item.id !== id));
   };
   const addCard = (newData) => {
+    setOriginalData((prevData) => [...prevData, newData]);
     setData((prevData) => [...prevData, newData]);
   };
-
   const editCard = (id) => {
     setData((prevData) =>
       prevData.map((item) =>
@@ -22,12 +30,11 @@ export default function App() {
       )
     );
   };
-
   const handleSearch = (searchTerm) => {
-    if (searchTerm.trim() === "") {
-      setData(cardData);
+    if (searchTerm.trim() === '') {
+      setData(originalData);
     } else {
-      const filteredCards = data.filter((item) =>
+      const filteredCards = originalData.filter((item) =>
         item.body.toLowerCase().includes(searchTerm.toLowerCase())
       );
       setData(filteredCards);
@@ -35,24 +42,33 @@ export default function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <AddButton addCard={addCard} data={data} />
-        <img src={logo} width={30} height={30} alt="techstack media logo" />
-        <Search handleSearch={handleSearch} />
-      </header>
-      <main className="App-main">
-        {data.map((item) => (
-          <Card
-            key={item.id}
-            id={item.id}
-            item={item}
-            removeCard={removeCard}
-            editCard={editCard}
-            isBlur={item.isBlur}
-          />
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <AddButton addCard={addCard} data={data} />
+          <img src={logo} width={30} height={30} alt='techstack media logo' />
+          <Search handleSearch={handleSearch} />
+        </header>
+        <main className='App-main'>
+          {currentItems.map((item) => (
+            <Card
+              key={item.id}
+              id={item.id}
+              item={item}
+              removeCard={removeCard}
+              editCard={editCard}
+              isBlur={item.isBlur}
+            />
+          ))}
+        </main>
+      </div>
+      <div className='App-pagination'>
+        {[...Array(Math.ceil(data.length / ITEMS_PER_PAGE))].map((_, index) => (
+          <button key={index} onClick={() => paginate(index + 1)}>
+            {index + 1}
+          </button>
         ))}
-      </main>
-    </div>
+      </div>
+    </>
   );
 }
